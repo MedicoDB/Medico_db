0b24c41a7e23a9f8303a2023203a515c2ab8c0cf
AbdulsametEkinci <ekincia22@itu.edu.tr>
2025-11-05 13:18:24 +0300

    Setting up the Database, Readme added and tested api for patients, dashboard

---
+++ backend/app/__init__.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask_cors import CORS
+
+def create_app():
+    app = Flask(__name__)
+    CORS(app)
+
+    from .api.patients import bp as patients_bp
+    app.register_blueprint(patients_bp, url_prefix="/api/patients")
+
+    from .api.dashboard import bp as dashboard_bp
+    app.register_blueprint(dashboard_bp, url_prefix="/api/dashboard")
+
+    return app
